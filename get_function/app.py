7e0a3f25447b3ff7889feef85e43b50a27b2a1e2
phd <philenko.d@gmail.com>
2021-05-13 22:50:39 -0400

    read from dynamo and inline policy added

--- get_function/app.py
+++ get_function/app.py
@@ -1,47 +1,33 @@
 import json
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table_name = 'cloud-resume'
+table = dynamodb.Table(table_name)
+ID = '0'
 
 # import requests
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*'
-        },
-        "body": json.dumps({
-            "counter": "2",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    print('Fetching item id = {} from the dB\n'.format(id))
+    
+    response = table.get_item(
+        Key={
+            'ID': ID
+        }
+    )
+        
+    if 'Item' in response:
+        return {
+            "statusCode": 200,
+            'headers': {
+                        'Access-Control-Allow-Headers': '*',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'GET'
+            },
+            "body": json.dumps({
+                        "counter": response['Item'].get('counter'),
+            }),
+        }
+    else:
+            print('Item id = {} not found in the dB'.format(id))
